dba4a0ff2e0968b56c037b74bf39445cf8095da0
Raghav2709 <kakraniachirag@gmail.com>
2024-06-13 00:10:45 +0530

    drk mode added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ThemeProvider } from "next-themes";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Header/>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+        <Header/>
+        {children}
+        </Providers>
+        </body>
     </html>
   );
 }
