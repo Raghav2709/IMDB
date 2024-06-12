6bffcc38b7839f9c905cc4dae22215fd5a53f408
Raghav2709 <kakraniachirag@gmail.com>
2024-06-13 01:05:43 +0530

    data fetched from tmdb

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,17 +1,25 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
-import { useSearchParams } from 'next/navigation'
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useSearchParams } from "next/navigation";
 
-const NavbarItem = ({title, param}) => {
-
-    const searchParams = useSearchParams();
-    const genre = searchParams.get('genre')
+const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
   return (
     <div>
-        <Link className={`hover: text-amber-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : '' }`} href={`/?genre=${param}`}>{title}</Link>
+      <Link
+        className={`hover: text-amber-600 font-semibold ${
+          genre === param
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarItem
+export default NavbarItem;
